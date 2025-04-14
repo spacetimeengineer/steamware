2938814ee0d3dad27e1183bdb0a1753b395ba101
Michael C Ryan <spacetime.engineer@gmail.com>
2025-04-14 10:07:59 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="steamware",
+    version="0.1.0",
+    author="Your Name",
+    author_email="your_email@example.com",
+    description="A modular part family and hardware assembly language for 3D printing.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/spacetimeengineer/STEAMWare",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "loguru",
+    ],
+    entry_points={
+        "console_scripts": [
+            "steamware=steamware:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
